7f4746a11508db739cd50318e82690825afd420a
ZidanErnandiaz <zidanecool1234@gmail.com>
2023-11-16 20:40:42 +0700

    -

---
+++ Kode Sumber/aboutus/src/screens/Index/Index.jsx
@@ -0,0 +1,347 @@
+import React from "react";
+import "./style.css";
+
+export const Index = () => {
+  return (
+    <div className="index">
+      <div className="div">
+        <footer className="footer">
+          <div className="content">
+            <div className="top">
+              <img
+                className="social-media-icon"
+                alt="Social media icon"
+                src="https://cdn.animaapp.com/projects/65322e0cd2f69387b8ca88c4/releases/6555a2fb16d3217e1f53ef02/img/social-media-icon-1.svg"
+              />
+              <div className="logo">
+                <div className="overlap-group">
+                  <img
+                    className="group"
+                    alt="Group"
+                    src="https://cdn.animaapp.com/projects/65322e0cd2f69387b8ca88c4/releases/6555a2fb16d3217e1f53ef02/img/group@2x.png"
+                  />
+                  <img
+                    className="img"
+                    alt="Group"
+                    src="https://cdn.animaapp.com/projects/65322e0cd2f69387b8ca88c4/releases/6555a2fb16d3217e1f53ef02/img/group-1@2x.png"
+                  />
+                  <img
+                    className="group-2"
+                    alt="Group"
+                    src="https://cdn.animaapp.com/projects/65322e0cd2f69387b8ca88c4/releases/6555a2fb16d3217e1f53ef02/img/group-2@2x.png"
+                  />
+                  <img
+                    className="group-3"
+                    alt="Group"
+                    src="https://cdn.animaapp.com/projects/65322e0cd2f69387b8ca88c4/releases/6555a2fb16d3217e1f53ef02/img/group-3@2x.png"
+                  />
+                  <img
+                    className="group-4"
+                    alt="Group"
+                    src="https://cdn.animaapp.com/projects/65322e0cd2f69387b8ca88c4/releases/6555a2fb16d3217e1f53ef02/img/group-4@2x.png"
+                  />
+                  <img
+                    className="group-5"
+                    alt="Group"
+                    src="https://cdn.animaapp.com/projects/65322e0cd2f69387b8ca88c4/releases/6555a2fb16d3217e1f53ef02/img/group-5@2x.png"
+                  />
+                  <img
+                    className="group-6"
+                    alt="Group"
+                    src="https://cdn.animaapp.com/projects/65322e0cd2f69387b8ca88c4/releases/6555a2fb16d3217e1f53ef02/img/group-6@2x.png"
+                  />
+                  <img
+                    className="group-7"
+                    alt="Group"
+                    src="https://cdn.animaapp.com/projects/65322e0cd2f69387b8ca88c4/releases/6555a2fb16d3217e1f53ef02/img/group-7@2x.png"
+                  />
+                  <div className="group-8" />
+                </div>
+              </div>
+              <img
+                className="social-media-icon"
+                alt="Social media icon"
+                src="https://cdn.animaapp.com/projects/65322e0cd2f69387b8ca88c4/releases/6555a2fb16d3217e1f53ef02/img/social-media-icon-2.svg"
+              />
+            </div>
+            <div className="middle">
+              <div className="contact">
+                <div className="sub-title">
+                  <img
+                    className="line"
+                    alt="Line"
+                    src="https://cdn.animaapp.com/projects/65322e0cd2f69387b8ca88c4/releases/6555a2fb16d3217e1f53ef02/img/line-16.svg"
+                  />
+                  <div className="text-wrapper">CONTACT</div>
+                  <img
+                    className="line-2"
+                    alt="Line"
+                    src="https://cdn.animaapp.com/projects/65322e0cd2f69387b8ca88c4/releases/6555a2fb16d3217e1f53ef02/img/line-16.svg"
+                  />
+                </div>
+                <p className="jl-bengawan-no">
+                  <span className="span">
+                    Jl. Bengawan No.67, Bandung
+                    <br />
+                  </span>
+                  <span className="text-wrapper-2">Call</span>
+                  <span className="span">
+                    {" "}
+                    - +62 817 0288 799 <br />
+                  </span>
+                  <span className="text-wrapper-2">bebekayayo@gmail.com </span>
+                </p>
+              </div>
+              <div className="newslatter">
+                <p className="join-our-mailing">
+                  {" "}
+                  Join our mailing list for updates,
+                  <br />
+                  Get news &amp; offers events.
+                </p>
+                <button className="button">
+                  <div className="email">
+                    <input className="input" placeholder="Email" type="email" />
+                  </div>
+                  <button className="div-wrapper">
+                    <div className="text-wrapper-3">Subscribe</div>
+                  </button>
+                </button>
+              </div>
+              <div className="working-hour">
+                <div className="sub-title-2">
+                  <img
+                    className="line-3"
+                    alt="Line"
+                    src="https://cdn.animaapp.com/projects/65322e0cd2f69387b8ca88c4/releases/6555a2fb16d3217e1f53ef02/img/line-18.svg"
+                  />
+                  <div className="text-wrapper">WORKING HOURS</div>
+                  <img
+                    className="line-4"
+                    alt="Line"
+                    src="https://cdn.animaapp.com/projects/65322e0cd2f69387b8ca88c4/releases/6555a2fb16d3217e1f53ef02/img/line-18.svg"
+                  />
+                </div>
+                <p className="mon-thu">
+                  <span className="text-wrapper-2">Mon – Thu</span>
+                  <span className="span">
+                    : 11.00am – 9.30pm
+                    <br />
+                  </span>
+                  <span className="text-wrapper-2">Sat</span>
+                  <span className="span">
+                    : 11.00am – 9.30pm
+                    <br />
+                  </span>
+                  <span className="text-wrapper-2">Fri</span>
+                  <span className="span">: 1.00am – 9.30pm</span>
+                </p>
+              </div>
+            </div>
+            <div className="bottom">
+              <img
+                className="line-5"
+                alt="Line"
+                src="https://cdn.animaapp.com/projects/65322e0cd2f69387b8ca88c4/releases/6555a2fb16d3217e1f53ef02/img/line-20.svg"
+              />
+              <div className="copyright">
+                <p className="copyright-bebek">
+                  <span className="span">© Copyright - Bebek Bakar Ayayo | Designed by </span>
+                  <span className="text-wrapper-2">Kelompok 9</span>
+                  <span className="span"> Templates - Powered by </span>
+                  <span className="text-wrapper-2">Figma</span>
+                </p>
+                <div className="styleguide-licenses">Styleguide /&nbsp;&nbsp; Licenses</div>
+              </div>
+            </div>
+          </div>
+        </footer>
+        <div className="counts">
+          <div className="overlap-group-wrapper">
+            <div className="overlap-group-2">
+              <img
+                className="line-6"
+                alt="Line"
+                src="https://cdn.animaapp.com/projects/65322e0cd2f69387b8ca88c4/releases/6555a2fb16d3217e1f53ef02/img/line-39.png"
+              />
+              <div className="content-2">
+                <div className="element">
+                  <img
+                    className="icon"
+                    alt="Icon"
+                    src="https://cdn.animaapp.com/projects/65322e0cd2f69387b8ca88c4/releases/6555a2fb16d3217e1f53ef02/img/icon.svg"
+                  />
+                  <div className="content-3">
+                    <div className="text-wrapper-4">Fresh Product</div>
+                    <p className="professional">
+                      Professional consider everyone
+                      <br />
+                      probls small niche friendly.
+                    </p>
+                  </div>
+                  <div className="text-wrapper-5">See more</div>
+                </div>
+                <div className="element">
+                  <img
+                    className="icon-2"
+                    alt="Icon"
+                    src="https://cdn.animaapp.com/projects/65322e0cd2f69387b8ca88c4/releases/6555a2fb16d3217e1f53ef02/img/icon-1.svg"
+                  />
+                  <div className="content-3">
+                    <div className="text-wrapper-4">Skilled Chefs</div>
+                    <p className="professional">
+                      Professional consider everyone
+                      <br />
+                      probls small niche friendly.
+                    </p>
+                  </div>
+                  <div className="text-wrapper-5">See more</div>
+                </div>
+                <div className="element">
+                  <img
+                    className="icon-3"
+                    alt="Icon"
+                    src="https://cdn.animaapp.com/projects/65322e0cd2f69387b8ca88c4/releases/6555a2fb16d3217e1f53ef02/img/icon-2.svg"
+                  />
+                  <div className="content-3">
+                    <div className="drinks-juices">Drinks &amp; Juices</div>
+                    <p className="professional">
+                      Professional consider everyone
+                      <br />
+                      probls small niche friendly.
+                    </p>
+                  </div>
+                  <div className="text-wrapper-5">See more</div>
+                </div>
+                <div className="element">
+                  <img
+                    className="icon-3"
+                    alt="Icon"
+                    src="https://cdn.animaapp.com/projects/65322e0cd2f69387b8ca88c4/releases/6555a2fb16d3217e1f53ef02/img/icon-3.svg"
+                  />
+                  <div className="content-3">
+                    <div className="text-wrapper-6">Vegan Cuisine</div>
+                    <p className="professional">
+                      Professional consider everyone
+                      <br />
+                      probls small niche friendly.
+                    </p>
+                  </div>
+                  <div className="text-wrapper-5">See more</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="about">
+          <div className="image">
+            <div className="overlap-group-3">
+              <div className="pattern" />
+              <img
+                className="image-2"
+                alt="Image"
+                src="https://cdn.animaapp.com/projects/65322e0cd2f69387b8ca88c4/releases/6555a2fb16d3217e1f53ef02/img/image-13.png"
+              />
+            </div>
+          </div>
+          <div className="content-wrapper">
+            <div className="content-4">
+              <div className="content-5">
+                <div className="sub-title-3">
+                  <img
+                    className="line-7"
+                    alt="Line"
+                    src="https://cdn.animaapp.com/projects/65322e0cd2f69387b8ca88c4/releases/6555a2fb16d3217e1f53ef02/img/line-40.svg"
+                  />
+                  <div className="about-us">ABOUT US</div>
+                  <img
+                    className="line-8"
+                    alt="Line"
+                    src="https://cdn.animaapp.com/projects/65322e0cd2f69387b8ca88c4/releases/6555a2fb16d3217e1f53ef02/img/line-40.svg"
+                  />
+                </div>
+                <div className="content-6">
+                  <div className="quality-and">Quality and&nbsp;&nbsp;Tradition</div>
+                  <p className="ayayo-bandung-s">
+                    Ayayo Bandung&#39;s Grilled Duck is a mouthwatering Indonesian culinary delight that will tantalize
+                    your taste buds with its exquisite flavors and textures. This dish features succulent duck,
+                    marinated in a medley of aromatic spices, herbs, and seasonings, creating a harmonious fusion of
+                    sweet, savory, and slightly spicy flavors. The duck is expertly grilled to perfection, imparting a
+                    smoky and charred essence to the tender meat. <br /> <br />
+                    Served with a side of steamed rice and traditional Indonesian sambal, Ayayo Bandung&#39;s Grilled
+                    Duck offers a delightful balance of flavors and textures. The sambal, made from a blend of chilies,
+                    shallots, and various ingredients, adds a zesty and fiery kick to the dish, complementing the
+                    richness of the duck. <br /> <br />
+                    This delectable dish not only captures the essence of Indonesian cuisine but also showcases the
+                    culinary expertise of Ayayo Bandung, making it a must-try for food enthusiasts looking to savor the
+                    unique and savory flavors of grilled duck in Bandung.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="menu-title">
+            <img
+              className="line-9"
+              alt="Line"
+              src="https://cdn.animaapp.com/projects/65322e0cd2f69387b8ca88c4/releases/6555a2fb16d3217e1f53ef02/img/line-42.svg"
+            />
+            <div className="text-wrapper-7">About Us</div>
+            <img
+              className="line-10"
+              alt="Line"
+              src="https://cdn.animaapp.com/projects/65322e0cd2f69387b8ca88c4/releases/6555a2fb16d3217e1f53ef02/img/line-42.svg"
+            />
+          </div>
+          <header className="header">
+            <div className="logo-2">
+              <button className="button-2">
+                <div className="text-wrapper-8">Call - 0817-0288-799</div>
+              </button>
+              <img
+                className="logo-3"
+                alt="Logo"
+                src="https://cdn.animaapp.com/projects/65322e0cd2f69387b8ca88c4/releases/6555a2fb16d3217e1f53ef02/img/logo-2.svg"
+              />
+              <button className="button-3">
+                <img
+                  className="cart"
+                  alt="Cart"
+                  src="https://cdn.animaapp.com/projects/65322e0cd2f69387b8ca88c4/releases/6555a2fb16d3217e1f53ef02/img/cart@2x.png"
+                />
+                <button className="button-4">
+                  <div className="text-wrapper-9">Reservation</div>
+                </button>
+              </button>
+            </div>
+            <div className="menu">
+              <img
+                className="line-11"
+                alt="Line"
+                src="https://cdn.animaapp.com/projects/65322e0cd2f69387b8ca88c4/releases/6555a2fb16d3217e1f53ef02/img/line-14.svg"
+              />
+              <div className="content-7">
+                <div className="homes-about-menu">
+                  Homes&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;About&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Menu&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Reservation&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Contact&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Delivery
+                  Order
+                </div>
+                <img
+                  className="social-media-icon-2"
+                  alt="Social media icon"
+                  src="https://cdn.animaapp.com/projects/65322e0cd2f69387b8ca88c4/releases/6555a2fb16d3217e1f53ef02/img/social-media-icon.svg"
+                />
+              </div>
+              <img
+                className="line-12"
+                alt="Line"
+                src="https://cdn.animaapp.com/projects/65322e0cd2f69387b8ca88c4/releases/6555a2fb16d3217e1f53ef02/img/line-14.svg"
+              />
+            </div>
+          </header>
+        </div>
+      </div>
+    </div>
+  );
+};
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # PBW2
-Assignment3 Muhammad Zidan Ernandiaz 6706223117
+- Rafa Suryaputra - 6706223162
+- Wijdan Ardra Fulvian - 6706223137
+- Muhammad Zidan Ernandiaz - 6706223117
